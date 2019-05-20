7b2a16e38e75961ea6c577b2c715e401cafcc47b
Afaq Anwar <aanwar9020@bths.edu>
2019-05-20 09:25:30 -0400

    steptracker done
    Signed-off-by: sheba sow <ssow8171@bths.edu>

---
+++ src/StepTracker.java
@@ -0,0 +1,29 @@
+public class StepTracker {
+    private int totalSteps;
+    private int activeDays;
+    private int totalDays;
+    private int limit;
+
+    public StepTracker(int limit) {
+        this.limit = limit;
+        totalDays = 0;
+        totalSteps = 0;
+        activeDays = 0;
+    }
+
+    public void addDailySteps(int count) {
+        totalSteps += count;
+        if (count > limit) {
+            activeDays++;
+        }
+        totalDays++;
+    }
+
+    public int activeDays() {
+        return activeDays;
+    }
+
+    public double averageSteps() {
+        return totalSteps / totalDays;
+    }
+}
